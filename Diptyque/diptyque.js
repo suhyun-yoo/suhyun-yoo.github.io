fe2491039417e605b5e902d94ec91768d4a0f0dc
suhyun-yoo <wau3384@gmail.com>
2023-01-07 20:41:46 +0900

    jquery 문법 전환 중

--- Diptyque/diptyque.js
+++ Diptyque/diptyque.js
@@ -1,3 +1,9 @@
 function changeColor(){
     document.getElementById('icon').src = './img/heart_color.png';
 }
+
+// $(document).ready(function(){
+//     $('.icon').click(function(){
+//         $(this).attr(src) = url;
+//     })
+// });
