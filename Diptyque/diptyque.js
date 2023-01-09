2b85f9ede66ad4cdbfe170f8345f9e7f4cd14c99
suhyun-yoo <wau3384@gmail.com>
2023-01-09 18:44:38 +0900

    파일 정리

--- Diptyque/diptyque.js
+++
@@ -1,9 +0,0 @@
-function changeColor(){
-    document.getElementById('icon').src = './img/heart_color.png';
-}
-
-// $(document).ready(function(){
-//     $('.icon').click(function(){
-//         $(this).attr(src) = url;
-//     })
-// });
