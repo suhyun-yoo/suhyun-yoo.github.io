62fedb206332d3c23b958e2efaebbed5f0083bd2
suhyun-yoo <wau3384@gmail.com>
2023-01-26 21:05:25 +0900

    기능 설명 주석 수정

--- PUIS/js/script.js
+++ PUIS/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    // 풀페이지 연결
+    // ===== 풀페이지 연결
     new fullpage('#wrap', {
         // 새로고침 시, 최상단 section으로 이동되는 부분 막기
         anchors: ['banner','puddingMenu', 'financierMenu'],
@@ -9,7 +9,7 @@ $(document).ready(function(){
         fitToSection : false
     });
 
-    // 위치값에 따라 scroll 버튼 보이기 및 감추기
+    // ===== 위치값에 따라 scroll 버튼 보이기 및 감추기
     $(window).scroll(function(){
         let sct = $(this).scrollTop();
         const sec3 = $('.sec2').offset().top;
@@ -20,10 +20,10 @@ $(document).ready(function(){
         }
     });
 
-    // userBox
+    // ===== 로그인 및 회원가입 모달창
+    // user박스 기본값 
     $('.banner .userBox-wrap.loginBox').hide();
     $('.banner .userBox-wrap.signupBox').hide();
-
     // 1. icon-box 클릭 시, 로그인 창 보이기
     $('.banner .icon-box').click(function(){
         $('.banner .userBox-wrap.loginBox').slideDown();
@@ -57,12 +57,13 @@ $(document).ready(function(){
         alert('회원가입 완료! 로그인 후 사용해주세요')
     });
 
-    // menu-btn 클릭 시, 메뉴 보이기 및 닫기
+    // ===== menu-btn 클릭 시, 메뉴 보이기 및 닫기
     $('.banner header .menu-btn').click(function(){
         $(this).toggleClass('active');
         $('.banner .sub-menu').toggleClass("active");
     });
-    // sub-menu > menu 버튼 클릭 시, section1으로 이동하고 sub-menu 닫기
+
+    // ==== sub-menu > menu 버튼 클릭 시, section1으로 이동하고 sub-menu 닫기
     $('.banner .sub-menu').children().first().click(function(){
         let sec1Top = $('.sec1').offset().top;
         $('html, body').animate({
@@ -72,7 +73,7 @@ $(document).ready(function(){
         $('.banner header .menu-btn').removeClass('active');
     });
 
-    // section1 section2 영역에서는 header 스타일링 변경
+    // ===== section1 section2 영역에서는 header 스타일링 변경
     $(window).scroll(function(){
         sec1Top = $('.sec1').offset().top - 100;
         let sct = $(window).scrollTop();
@@ -87,7 +88,7 @@ $(document).ready(function(){
         }
     });
 
-    // pudding financier swiper
+    // ===== pudding financier swiper
     // window 크기값에 따라 siwper 크기 변경하기
     // 1. 함수 선언
     const swiperRe = () => {
@@ -156,10 +157,7 @@ $(document).ready(function(){
         let ww = $(window).width();
         swiperRe();
     });
-
-
-    // swiper 영역에 마우스 호버 시, 슬라이드 멈추고
-    // 마우스가 떠나면 다시 재생하기
+    // swiper 영역에 마우스 호버 시, 슬라이드 멈추고 마우스가 떠나면 다시 재생하기
     $(".mySwiper").each(function(elem, target){
         var swp = target.swiper;
         $(this).hover(function() {
@@ -169,7 +167,7 @@ $(document).ready(function(){
         });
     });
 
-    // 버튼 클릭 시, 메뉴 소개 영역 보이기
+    // ===== 버튼 클릭 시, 메뉴 소개 영역 보이기
     $('.banner header .logo').click(function(){
         $('.sec3').show();
     });
